914a9709257d1db95672f4028ae1993688ed894d
Ivan Ortega Salas <iportega@yahoo.com>
2016-09-28 22:48:52 -0500

    Comentario

--- Hola Mundo/ViewController.swift
+++ Hola Mundo/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var messageLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changeMessage(sender: AnyObject) {
+        messageLabel.text="Hola \(nameTextField.text!)"
+        view.endEditing(true)
+    }
+    //Commentary
 
 }
 
